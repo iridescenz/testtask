b9c9a7e1bc0184fc33ae5b3e4bb588fb9bd8f387
Irina Turenko <turenko7irina@mail.ru>
2021-05-18 11:23:29 +0300

    files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Page1 from './components/Page1'
 
 function App() {
   return (
-
-  );
+  <Page1 />
+  )
 }
 
 export default App;
