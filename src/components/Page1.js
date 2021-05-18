aae358e53aa1f75b0b2dba519c39d2b29a9aa7d4
Irina Turenko <turenko7irina@mail.ru>
2021-05-18 18:27:26 +0300

    upd

--- src/components/Page1.js
+++ src/components/Page1.js
@@ -12,8 +12,8 @@ const Page1 = () => {
 
   const [slots, setSlots] = useState('');
   const [step, setStep] = useState("1/3");
-  const [plan, setPlan] = useState('Стандарт');
-  const [price, setPrice] = useState(null);
+  const [plan, setPlan] = useState(null);
+  const [price, setPrice] = useState(0);
 
   return (
     <div className='container'>
@@ -25,11 +25,16 @@ const Page1 = () => {
               header={el.header}
               price={el.price}
               key={el.ind}
-              onClick={(e) => {
+              onClick={async(e) => {
                 e.preventDefault();
                 setPlan(el.header);
-              }}
-            />
+                const element = plans.filter(el => el.name == plan);
+                element.length > 0  && setPrice(element[0].price);
+                element.length > 0  && setSlots(element[0].slots);
+                // let result =  new Promise ((resolve, reject) => resolve (plans.filter(el => el.name === plan)));
+                // const elem = await result.then();
+
+                }} />
           ))}
         </div>
         <form className='form'>
@@ -78,14 +83,15 @@ const Page1 = () => {
         <h2>Ваш запрос</h2>
         <div className='plan-name'>
           {plan === null ? <div>План не выбран</div> : `План "${plan}"`}
+          {() => setPrice(price)}
           { price === 0 ? <div> 0 </div> : <div>{price} </div>}
         </div>
-        <div>Итого:</div>
+        <div>Итого: {price * slots}</div>
         <div className='balance'>
           <div>Ваш баланс:{` ${user.balance}`}</div>
           <div className='footer'>
             <button className='balance-check'>Запросить</button> 
-            <h1>{ plan}</h1>
+            <h1>{plan}</h1>
           </div>
         </div>
       </div>
